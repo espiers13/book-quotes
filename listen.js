8dbba4eb37d1babfa439c88952644a93f093edaa
Emily <e.spiers13@gmail.com>
2025-03-30 15:51:55 +0100

    debugging for fe access

--- listen.js
+++ listen.js
@@ -1,9 +1,11 @@
 const app = require("./app");
 
-const { PORT = 9090 } = process.env;
+const { PORT = 3000 } = process.env;
 
-app.listen(PORT, (err) => {
+app.listen(PORT, "0.0.0.0", (err) => {
   if (err) {
     console.log(err);
-  } else console.log(`listening on ${PORT}...`);
+  } else {
+    console.log(`Server listening on http://0.0.0.0:${PORT}...`);
+  }
 });
